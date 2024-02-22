39e695b1537c48e59aaf3aaedb4640106996008f
Lemar <abawi@cip.ifi.lmu.de>
2024-02-22 18:04:30 +0100

    funkt

--- src/cdcl/update.cpp
+++ src/cdcl/update.cpp
@@ -1,57 +1,67 @@
 #include "../../include/cnf2.hpp"
 
 void updateWatchedLiterals(int assertedVar) {
-    auto clausesToUpdatePointers = &vars[assertedVar].neg_watched;
+  auto clausesToUpdatePointers = &vars[assertedVar].neg_watched;
 
-    if (vars[assertedVar].getValue() == FALSE) {
-        clausesToUpdatePointers = &vars[assertedVar].pos_watched;
+  if (vars[assertedVar].getValue() == FALSE) {
+    clausesToUpdatePointers = &vars[assertedVar].pos_watched;
+  }
+
+  auto copy = *clausesToUpdatePointers;
+
+  for (auto clauseIndex = copy.begin(); clauseIndex != copy.end();
+       ++clauseIndex) {
+
+    std::vector<int> &clause = cnf[*clauseIndex];
+    bool found = false;
+
+    if (clause.size() == 1)
+      goto UP_Backtrack;
+
+    if (eval(clause[0]) || eval(clause[1]))
+      continue;
+
+    // swap false literal to index 1
+    if (std::abs(clause[1]) != assertedVar) {
+      int tmp = clause[1];
+      clause[1] = clause[0];
+      clause[0] = tmp;
     }
 
-    auto copy = *clausesToUpdatePointers;
+    for (int i = 2; i < clause.size(); i++) {
+      if (eval(clause[i]) || vars[std::abs(clause[i])].getValue() == FREE) {
+        int tmp = clause[i];
+        clause[i] = clause[1];
+        clause[1] = tmp;
 
-    for (auto clauseIndex = copy.begin(); clauseIndex != copy.end(); ++clauseIndex) {
-        std::vector<int>& clause = cnf[*clauseIndex];
-        if (eval(clause[0]) || eval(clause[1])) continue;
-
-        // swap false literal to index 1
-        if (std::abs(clause[1]) != assertedVar) {
-            int tmp = clause[1];
-            clause[1] = clause[0];
-            clause[0] = tmp;
-        }
-
-        bool found = false;
-
-        for (int i = 2; i < clause.size(); i++) {
-            if (eval(clause[i]) || vars[std::abs(clause[i])].getValue() == FREE) {
-                int tmp = clause[i];
-                clause[i] = clause[1];
-                clause[1] = tmp;
-
-                clausesToUpdatePointers->erase(*clauseIndex);
-                tmp > 0 ? vars[std::abs(tmp)].pos_watched.insert(*clauseIndex)
-                        : vars[std::abs(tmp)].neg_watched.insert(*clauseIndex);
-                found = true;
-                break;
-            }
-        }
-
-        if (found) continue;
-
-        // Search for a distinct new pointer unsuccessful, try UP on otherPointer else backtrack
-        int otherPointer = clause[0];
-        if (vars[std::abs(otherPointer)].getValue() == FREE) {
-            if (!vars[std::abs(otherPointer)].enqueued) {
-                unitQueue.push(otherPointer);
-                vars[std::abs(otherPointer)].enqueued = true;
-            }
-        } else {
-            backtrack();
-            return;
-        }
+        clausesToUpdatePointers->erase(*clauseIndex);
+        tmp > 0 ? vars[std::abs(tmp)].pos_watched.insert(*clauseIndex)
+                : vars[std::abs(tmp)].neg_watched.insert(*clauseIndex);
+        found = true;
+        break;
+      }
     }
 
-    if (numOfUnassigned == 0 && unitQueue.empty()) pthread_exit(0);
+  UP_Backtrack:
+    if (found)
+      continue;
+
+    int otherPointer = clause[0];
+    if (vars[std::abs(otherPointer)].getValue() == FREE) {
+      if (!vars[std::abs(otherPointer)].enqueued) {
+        unitQueue.push(otherPointer);
+        vars[std::abs(otherPointer)].enqueued = true;
+      }
+    } else {
+      backtrack();
+      return;
+    }
+  }
+
+  if (numOfUnassigned == 0 && unitQueue.empty())
+    pthread_exit(0);
 }
 
-bool eval(int literal) { return !(vars[std::abs(literal)].getValue() ^ (literal > 0)); }
+bool eval(int literal) {
+  return !(vars[std::abs(literal)].getValue() ^ (literal > 0));
+}
