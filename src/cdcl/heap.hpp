8942a322775d0c55b1c4083920da463fc0c5210e
saygac <saygac@cip.ifi.lmu.de>
2024-02-25 22:49:48 +0100

    vsids heap part 1

---
+++ src/cdcl/heap.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+
+struct VSIDSInfo {
+
+    double score;
+    double decay;
+};
+
+std::unordered_map<int, VSIDSInfo> vsidsInfo; 
+
+struct CompVSIDSScore{
+    bool operator()(int v1, int v2) const {
+        
+        return vsidsInfo[v1].score < vsidsInfo[v2].score;
+    }
+};
+
+std::priority_queue<int, std::vector<int>, CompVSIDSScore> vsidsPrioQueue;
