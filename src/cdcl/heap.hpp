87bef297cb919d75dcc4353f3613094864536c7b
saygac <saygac@cip.ifi.lmu.de>
2024-02-26 17:46:10 +0100

    A bit rounded out but still crude

--- src/cdcl/heap.hpp
+++ src/cdcl/heap.hpp
@@ -1,58 +1,80 @@
-#include <iostream>
 #include <climits>
-#include <vector>
+#include <iostream>
 #include <queue>
 #include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <stdlib.h>
+#include <math.h>
 
 #ifndef K_Heap_hpp
 #define K_Heap_hpp
 
 // A MinHeap for VSIDS Branching Heuristic
 
-template <class T>
-class Heap{ 
-    std::vector<T> heap;    
-    double score;  
-    double decay;
 
-    bool scoreOperator()(int v1, int v2) const {
-        
-        return vsidsInfo[v1].score < vsidsInfo[v2].score;
+class Heap {
+private:
+    int size;
+    int capacity;
+    std::vector<int> heap;                              // int vector for the order of elems
+    std::vector<double> score;                          // double vector for the scores of elems. score[1] == score of the 1st elem
+    int getLChild(int i) const { return i * 2 + 1; }
+    int getRChild(int i) const { return (i + 1) * 2; }
+    int getParent(int i) const { return (i - 1) / 2; }
+
+    bool scoreOperator(int v1, int v2) const { 
+        return score[v1] < score[v2]; 
     }
+
+    
+public:
+    Heap(int capacity);
 
     void heapifyUp(int);
     void heapifyDown(int);
-
-     
-public: 
-
-    Heap(){}
-
-    int getLChild (int i) const {return i*2+1;}
-    int getRChild (int i) const {return (i+1)*2;}
-    int getParent (int i) const {return (i-1) / 2;}
-
-    void insertKey(T);
+    void insertKey(int);
     int getHeapSize() const;
     void display() const;
-    T deleteKey();    
-
-
+    int deleteKey();
 };
 
-template<class T>
-void Heap<T>::heapifyUp(int c) {
+//Constructor
+Heap::Heap(int capacity){ 
+    size = 0;
+    this -> capacity = capacity;
+    heap.resize(capacity);
+};
 
-    int tmp = heap[c];
-    int p = getParent(c);
-
-    while (c != 0 && scoreOperator(tmp, heap[p])){
-        heap[c] = heap[p];
-
-    }
+void Heap::insertKey(int k) {
     
-}
+    //Increase amount of elems.
+    size++;
 
+    //Insert new key at the end
+    int i = size - 1;
+    heap[i] = k;
+    
+    //Fix the tree property
+    //Move the element up until i => parent or root
+    while (i != 0 && score[getParent(i) > score[i]]) {
+        std::swap(heap[i], heap[getParent(i)]);
+        i = getParent(i);
+    }
+}   
+
+
+
+
+void Heap::heapifyUp(int c) {
+
+  int tmp = heap[c];
+  int p = getParent(c);
+
+  while (c != 0 && scoreOperator(tmp, heap[p])) {
+    heap[c] = heap[p];
+  }
+}
 
 // Unfinished
 void InitVSIDSScore(std::vector<int>);
