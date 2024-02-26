7a755fc00157dade75812f382ecbf27dc9653e3e
saygac <saygac@cip.ifi.lmu.de>
2024-02-26 16:47:26 +0100

    checkpoint heap

--- src/cdcl/heap.hpp
+++ src/cdcl/heap.hpp
@@ -4,22 +4,54 @@
 #include <queue>
 #include <unordered_map>
 
-struct VSIDSInfo {
+#ifndef K_Heap_hpp
+#define K_Heap_hpp
 
-    double score;
+// A MinHeap for VSIDS Branching Heuristic
+
+template <class T>
+class Heap{ 
+    std::vector<T> heap;    
+    double score;  
     double decay;
-};
 
-std::unordered_map<int, VSIDSInfo> vsidsInfo; 
-
-struct CompVSIDSScore{
-    bool operator()(int v1, int v2) const {
+    bool scoreOperator()(int v1, int v2) const {
         
         return vsidsInfo[v1].score < vsidsInfo[v2].score;
     }
+
+    void heapifyUp(int);
+    void heapifyDown(int);
+
+     
+public: 
+
+    Heap(){}
+
+    int getLChild (int i) const {return i*2+1;}
+    int getRChild (int i) const {return (i+1)*2;}
+    int getParent (int i) const {return (i-1) / 2;}
+
+    void insertKey(T);
+    int getHeapSize() const;
+    void display() const;
+    T deleteKey();    
+
+
 };
 
-std::priority_queue<int, std::vector<int>, CompVSIDSScore> vsidsPrioQueue;
+template<class T>
+void Heap<T>::heapifyUp(int c) {
+
+    int tmp = heap[c];
+    int p = getParent(c);
+
+    while (c != 0 && scoreOperator(tmp, heap[p])){
+        heap[c] = heap[p];
+
+    }
+    
+}
 
 
 // Unfinished
@@ -27,3 +59,4 @@ void InitVSIDSScore(std::vector<int>);
 
 void UpdateVSIDSCore(int var);
 
+#endif
